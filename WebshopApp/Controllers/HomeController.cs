d5e40be011e2cc0a97d9ebfee9be867f480127ca
Jon Kristensen <jon@jonkr.dk>
2022-10-29 13:02:23 +0200

    Added a View named MyView

--- WebshopApp/Controllers/HomeController.cs
+++ WebshopApp/Controllers/HomeController.cs
@@ -15,17 +15,26 @@ namespace WebshopApp.Controllers
 
         public IActionResult Index()
         {
+            _logger.Log(LogLevel.Debug, "HomeController.Index()");
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.Log(LogLevel.Debug, "HomeController.Privacy()");
+            return View();
+        }
+
+        public IActionResult MyView()
+        {
+            _logger.Log(LogLevel.Debug, "HomeController.MyView()");
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            _logger.Log(LogLevel.Debug, "HomeController.Error()");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
